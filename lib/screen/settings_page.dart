7723aa86ec3b87ceb9422f9703015753fd0ce00f
Rivan8 <stevanustampi7@gmail.com>
2023-05-05 10:37:08 +0700

    Update settings_page.dart

--- lib/screen/settings_page.dart
+++ lib/screen/settings_page.dart
@@ -1,1 +1,12 @@
-  
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class settingPage extends StatelessWidget {
+  const settingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
